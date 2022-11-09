bb359d9efe3f6a0265114d1e0b9eab2ccaaae0bf
Kira-Floris <nzafloris@gmail.com>
2022-11-09 15:20:15 +0200

    adding python-multipart

--- omni.py
+++ omni.py
@@ -29,7 +29,7 @@ app.add_middleware(
 )
 
 templates = Jinja2Templates(directory='templates')
-app.mount('/static',StaticFiles(directory='static'), name='static')
+# app.mount('/static',StaticFiles(directory='static'), name='static')
 
 # @app.post('/image')
 # async def postImage(phone:str,file:UploadFile=File(...)):
